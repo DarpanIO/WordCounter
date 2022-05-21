2b21c2af8d9f82cd3a5f0ccfacd6783379d76c29
darpan9460@gmail.com <darpan9460@gmail.com>
2022-05-21 17:01:07 +0530

    Added clear button and fixed bug returning 1 word instead of 0

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,11 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText)
     }
+    const handleClearClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
+        let newText ="" ;
+        setText(newText)
+    }
 
     const handleOnChange = (event)=>{
         // console.log("On change");
@@ -21,6 +26,7 @@ export default function TextForm(props) {
     const [text, setText] = useState(''); 
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
+    var words=(text.split(" ").length===1 && text.length===0)?0:text.split(" ").length;
     return (
         <>
 
@@ -31,11 +37,12 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-3" onClick={handleDownClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-3" onClick={handleClearClick}>Clear</button>
         </div>
         <div className="container my-3">
         <h1>Summary</h1>
-        <p>{text.length} characters and {text.split(" ").length} words</p>
-        <p>{(text.split(" ").length)*0.008} minutes will needed to read</p>
+        <p>{text.length} characters and {words} words</p>
+        <p>{words*0.008} minutes will needed to read</p>
         </div>
         
         </>
